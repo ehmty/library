7aae7f2d7555e9d0c1e13388c6da9f51fad688ee
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-29 18:15:15 +0200

    add Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} yet`
+    }
+}
+
+
