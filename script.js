f90e65aa02c6f040d8f6e8e07bc3aab8ea3389d3
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-29 18:47:33 +0200

    add unique id to Book, add library array and a func to add more books to it

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} yet`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`
     }
 }
 
+const myLibrary = [];
 
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    return book;
+}
+
+const book1 = addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 295, true)
+const book2 = addBookToLibrary("The Game of Thrones", "George R. R. Martin", 694, false)
+
+console.log(book1.info());
+console.log(book2.info());
