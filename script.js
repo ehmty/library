233f51812da1fdfbe08cda4afc3bfc111ff8d630
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-30 14:29:21 +0200

    add event to button for adding a book to the table, change function and variable name

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.status = status;
     this.id = crypto.randomUUID();
 }
 
 const myLibrary = [];
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, status) {
+    const book = new Book(title, author, pages, status);
     myLibrary.push(book);
     return book;
 }
@@ -17,16 +17,17 @@ function addBookToLibrary(title, author, pages, read) {
 const book1 = addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 295, true)
 const book2 = addBookToLibrary("The Game of Thrones", "George R. R. Martin", 694, false)
 
-function showBooks(myLibrary) {
+function showLibrary(myLibrary) {
     const tableBody = document.querySelector("tbody");
+    tableBody.textContent = "";
     
     for (let book of myLibrary) {
         const tableRow = document.createElement("tr");
         
         for (let prop in book) {
             const tableData = document.createElement("td");
-            if (prop === "read") {
-                tableData.textContent = book.read ? "read": "not read yet";
+            if (prop === "status") {
+                tableData.textContent = book.status ? "read": "not read";
             } else {
                 tableData.textContent = book[prop];
             }
@@ -37,5 +38,21 @@ function showBooks(myLibrary) {
     }
 }
 
-showBooks(myLibrary);
+showLibrary(myLibrary);
 
+const form = document.querySelector("form");
+const dialog = document.querySelector("dialog");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const status = document.querySelector("input[type='radio']:checked").value === "true";
+
+    addBookToLibrary(title, author, pages, status);
+    showLibrary(myLibrary);
+
+    form.reset();
+})
